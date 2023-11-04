1a9c18920ca93119d9b079101678f37fb4e3492a
Šimon Brauner <simonbrauner@seznam.cz>
2023-11-04 21:46:47 +0100

    feat: day template

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::{fs::File, io::BufReader, path::PathBuf};
 use anyhow::{Context, Result};
 use clap::Parser;
 
+mod solutions;
+
 #[derive(Parser)]
 #[command(arg_required_else_help = true)]
 struct Args {
@@ -15,15 +17,17 @@ struct Args {
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let _reader = BufReader::new(
+    let reader = BufReader::new(
         File::open(&args.input)
             .with_context(|| format!("Failed to open '{}'", args.input.display()))?,
     );
 
-    println!(
-        "run solution {} on input file {}",
-        args.day,
-        args.input.display()
+    print!(
+        "{}",
+        match args.day {
+            1 => solutions::day_01::solve(reader)?,
+            _ => todo!(),
+        }
     );
 
     Ok(())
