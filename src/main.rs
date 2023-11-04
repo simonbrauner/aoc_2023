4ccaa30220477f05c554131f8b081401e74d32fb
Šimon Brauner <simonbrauner@seznam.cz>
2023-11-04 20:19:52 +0100

    feat: command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(arg_required_else_help = true)]
+struct Args {
+    /// Day of the puzzle, between 1 and 25
+    day: u8,
+
+    /// File to read input from
+    input: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!(
+        "run solution {} on input file {}",
+        args.day,
+        args.input.display()
+    );
 }
