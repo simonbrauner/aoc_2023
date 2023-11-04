7417eaaaf4a7bc5501e633b0e0e3e6315a942945
Šimon Brauner <simonbrauner@seznam.cz>
2023-11-04 22:23:56 +0100

    feat: call solve functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::BufReader, path::PathBuf};
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
 
 mod solutions;
@@ -26,7 +26,31 @@ fn main() -> Result<()> {
         "{}",
         match args.day {
             1 => solutions::day_01::solve(reader)?,
-            _ => todo!(),
+            2 => solutions::day_02::solve(reader)?,
+            3 => solutions::day_03::solve(reader)?,
+            4 => solutions::day_04::solve(reader)?,
+            5 => solutions::day_05::solve(reader)?,
+            6 => solutions::day_06::solve(reader)?,
+            7 => solutions::day_07::solve(reader)?,
+            8 => solutions::day_08::solve(reader)?,
+            9 => solutions::day_09::solve(reader)?,
+            10 => solutions::day_10::solve(reader)?,
+            11 => solutions::day_11::solve(reader)?,
+            12 => solutions::day_12::solve(reader)?,
+            13 => solutions::day_13::solve(reader)?,
+            14 => solutions::day_14::solve(reader)?,
+            15 => solutions::day_15::solve(reader)?,
+            16 => solutions::day_16::solve(reader)?,
+            17 => solutions::day_17::solve(reader)?,
+            18 => solutions::day_18::solve(reader)?,
+            19 => solutions::day_19::solve(reader)?,
+            20 => solutions::day_20::solve(reader)?,
+            21 => solutions::day_21::solve(reader)?,
+            22 => solutions::day_22::solve(reader)?,
+            23 => solutions::day_23::solve(reader)?,
+            24 => solutions::day_24::solve(reader)?,
+            25 => solutions::day_25::solve(reader)?,
+            _ => return Err(anyhow!("Invalid day: {}", args.day)),
         }
     );
 
