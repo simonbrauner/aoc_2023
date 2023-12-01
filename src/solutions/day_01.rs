6e8e4a2a1b06317a84219eaa3a5e5bbaa878048c
Šimon Brauner <simonbrauner@seznam.cz>
2023-12-01 08:26:50 +0100

    feat(01): part 2

--- src/solutions/day_01.rs
+++ src/solutions/day_01.rs
@@ -1,13 +1,26 @@
 pub fn solve(input: &[String]) -> String {
-    format!("{}\n{}\n", part_1(input), part_2())
+    format!("{}\n{}\n", part_1(input), part_2(input))
 }
 
 fn part_1(document: &[String]) -> u32 {
     calibration_sum(document)
 }
 
-fn part_2() -> String {
-    "part 2 unimplemented".to_string()
+fn part_2(document: &[String]) -> u32 {
+    let modified_document: Vec<String> = document
+        .iter()
+        .map(|line| {
+            let mut new_line = line.to_string();
+
+            for (word, digit) in WORDS_AND_DIGITS {
+                new_line = new_line.replace(word, &format!("{}{}{}", word, digit, word));
+            }
+
+            new_line
+        })
+        .collect();
+
+    calibration_sum(&modified_document)
 }
 
 fn calibration_sum(document: &[String]) -> u32 {
@@ -23,3 +36,15 @@ fn calibration_sum(document: &[String]) -> u32 {
         })
         .sum()
 }
+
+const WORDS_AND_DIGITS: [(&str, &str); 9] = [
+    ("one", "1"),
+    ("two", "2"),
+    ("three", "3"),
+    ("four", "4"),
+    ("five", "5"),
+    ("six", "6"),
+    ("seven", "7"),
+    ("eight", "8"),
+    ("nine", "9"),
+];
