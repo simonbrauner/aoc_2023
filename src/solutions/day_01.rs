05f1392417cdde2b84e4802f140e4a8e442b257e
Šimon Brauner <simonbrauner@seznam.cz>
2023-12-01 08:25:15 +0100

    feat(01): part 1

--- src/solutions/day_01.rs
+++ src/solutions/day_01.rs
@@ -1,11 +1,25 @@
-pub fn solve(_input: &[String]) -> String {
-    format!("{}\n{}\n", part_1(), part_2())
+pub fn solve(input: &[String]) -> String {
+    format!("{}\n{}\n", part_1(input), part_2())
 }
 
-fn part_1() -> String {
-    "part 1 unimplemented".to_string()
+fn part_1(document: &[String]) -> u32 {
+    calibration_sum(document)
 }
 
 fn part_2() -> String {
     "part 2 unimplemented".to_string()
+}
+
+fn calibration_sum(document: &[String]) -> u32 {
+    document
+        .iter()
+        .map(|line| {
+            let mut digits = line.chars().filter_map(|letter| letter.to_digit(10));
+
+            let first = digits.next().unwrap();
+            let last = digits.last().unwrap_or(first);
+
+            10 * first + last
+        })
+        .sum()
 }
