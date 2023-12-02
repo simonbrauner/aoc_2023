a52d0070bcbb70315d4ba79b952d63a80d3779f7
Šimon Brauner <simonbrauner@seznam.cz>
2023-12-02 09:35:44 +0100

    feat(02): part 1

--- src/solutions/day_02.rs
+++ src/solutions/day_02.rs
@@ -1,11 +1,74 @@
-pub fn solve(_input: &[String]) -> String {
-    format!("{}\n{}\n", part_1(), part_2())
+use regex::Regex;
+
+pub fn solve(input: &[String]) -> String {
+    let id_re = Regex::new(r"Game (\d+)").unwrap();
+    let cube_re = Regex::new(r"(\d+) ((?:red)|(?:green)|(?:blue))").unwrap();
+    let mut games = Vec::new();
+
+    for line in input {
+        let id = id_re.captures(line).unwrap()[1].parse().unwrap();
+        let mut cubes = Vec::new();
+
+        for (_, [count, color]) in cube_re.captures_iter(line).map(|c| c.extract()) {
+            cubes.push((
+                count.parse().unwrap(),
+                match color {
+                    "red" => CubeColor::Red,
+                    "green" => CubeColor::Green,
+                    "blue" => CubeColor::Blue,
+                    _ => panic!(),
+                },
+            ));
+        }
+
+        games.push(Game { id, cubes });
+    }
+
+    format!("{}\n{}\n", part_1(&games), part_2())
 }
 
-fn part_1() -> String {
-    "part 1 unimplemented".to_string()
+fn part_1(games: &[Game]) -> u32 {
+    games
+        .iter()
+        .filter_map(|game| {
+            if game.is_possible() {
+                Some(game.id)
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 fn part_2() -> String {
     "part 2 unimplemented".to_string()
+}
+
+struct Game {
+    id: u32,
+    cubes: Vec<(u32, CubeColor)>,
+}
+
+impl Game {
+    fn is_possible(&self) -> bool {
+        self.cubes
+            .iter()
+            .all(|(count, color)| count <= &color.maximal_count())
+    }
+}
+
+enum CubeColor {
+    Red,
+    Green,
+    Blue,
+}
+
+impl CubeColor {
+    fn maximal_count(&self) -> u32 {
+        match self {
+            CubeColor::Red => 12,
+            CubeColor::Green => 13,
+            CubeColor::Blue => 14,
+        }
+    }
 }
