8b16b55384082a303f9283e07f44eb5fe106271c
Šimon Brauner <simonbrauner@seznam.cz>
2023-12-05 10:00:11 +0100

    feat(03): part 2

--- src/solutions/day_03.rs
+++ src/solutions/day_03.rs
@@ -1,58 +1,97 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn solve(input: &[String]) -> String {
-    let mut engine = Vec::new();
+    let mut part_numbers: Vec<u32> = Vec::new();
+    let mut part_numbers_indices: HashMap<(usize, usize), usize> = HashMap::new();
+    let mut symbols: HashMap<(usize, usize), char> = HashMap::new();
 
-    for line in input {
-        engine.push(line.chars().collect())
-    }
-
-    format!("{}\n{}\n", part_1(&engine), part_2())
-}
-
-fn part_1(engine: &[Vec<char>]) -> u32 {
-    let mut part_number_sum = 0;
-
-    for (row, line) in engine.iter().enumerate() {
+    for (row, line) in input.iter().enumerate() {
         let mut number = 0;
-        let mut valid = false;
+        let mut length = 0;
+        let line: Vec<char> = line.chars().collect();
 
         for (col, letter) in line.iter().enumerate() {
             if let Some(digit) = letter.to_digit(10) {
                 number *= 10;
                 number += digit;
-
-                valid = valid || validity(engine, col, row);
+                length += 1;
 
                 if !line.get(col + 1).unwrap_or(&'.').is_ascii_digit() {
-                    if valid {
-                        part_number_sum += number;
+                    for col_decrement in 0..length {
+                        part_numbers_indices.insert((col - col_decrement, row), part_numbers.len());
                     }
 
+                    part_numbers.push(number);
                     number = 0;
-                    valid = false;
+                    length = 0;
                 }
+            } else if is_symbol(letter) {
+                symbols.insert((col, row), *letter);
             }
         }
     }
 
-    part_number_sum
+    let engine = Engine {
+        part_numbers,
+        part_numbers_indices,
+        symbols,
+    };
+
+    format!("{}\n{}\n", part_1(&engine), part_2(&engine))
 }
 
-fn part_2() -> String {
-    "part 2 unimplemented".to_string()
+fn part_1(engine: &Engine) -> u32 {
+    let mut indices: HashSet<usize> = HashSet::new();
+
+    for (col, row) in engine.symbols.keys() {
+        indices.extend(get_adjacent_indices(col, row, engine));
+    }
+
+    indices
+        .iter()
+        .map(|index| engine.part_numbers.get(*index).unwrap())
+        .sum()
+}
+
+fn part_2(engine: &Engine) -> u32 {
+    engine
+        .symbols
+        .iter()
+        .filter_map(|((col, row), symbol)| {
+            let adjacent_indices = get_adjacent_indices(col, row, engine);
+
+            if symbol == &'*' && adjacent_indices.len() == 2 {
+                Some(
+                    adjacent_indices
+                        .iter()
+                        .map(|index| engine.part_numbers.get(*index).unwrap())
+                        .product::<u32>(),
+                )
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+struct Engine {
+    part_numbers: Vec<u32>,
+    part_numbers_indices: HashMap<(usize, usize), usize>,
+    symbols: HashMap<(usize, usize), char>,
 }
 
 fn is_symbol(letter: &char) -> bool {
     !letter.is_ascii_digit() && letter != &'.'
 }
 
-fn validity(engine: &[Vec<char>], col_index: usize, row_index: usize) -> bool {
+fn get_adjacent_indices(col: &usize, row: &usize, engine: &Engine) -> HashSet<usize> {
     (-1..=1)
-        .filter_map(|row_change| row_index.checked_add_signed(row_change))
-        .filter_map(|row_index| engine.get(row_index))
-        .flat_map(|row| {
+        .filter_map(|col_change| col.checked_add_signed(col_change))
+        .flat_map(|col| {
             (-1..=1)
-                .filter_map(|col_change| col_index.checked_add_signed(col_change))
-                .filter_map(|col_index| row.get(col_index))
+                .filter_map(|row_change| row.checked_add_signed(row_change))
+                .filter_map(move |row| engine.part_numbers_indices.get(&(col, row)))
+                .copied()
         })
-        .any(is_symbol)
+        .collect()
 }
