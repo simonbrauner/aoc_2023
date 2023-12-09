667e19a492262c6b291e92814f4c103d25f737c0
Šimon Brauner <simonbrauner@seznam.cz>
2023-12-09 20:38:09 +0100

    feat(06): part 2

--- src/solutions/day_06.rs
+++ src/solutions/day_06.rs
@@ -3,39 +3,43 @@ use itertools::Itertools;
 pub fn solve(input: &[String]) -> String {
     let (times, records) = input
         .iter()
-        .map(|line| {
-            line.split_whitespace()
-                .skip(1)
-                .map(|number| number.parse().unwrap())
-                .collect::<Vec<u32>>()
-        })
+        .map(|line| line.split_whitespace().skip(1).collect::<Vec<&str>>())
         .collect_tuple()
         .unwrap();
 
     let races: Vec<Race> = times
-        .into_iter()
-        .zip(records)
-        .map(|(time, record)| Race { time, record })
+        .iter()
+        .zip(records.clone())
+        .map(|(time, record)| Race {
+            time: time.parse().unwrap(),
+            record: record.parse().unwrap(),
+        })
         .collect();
 
-    format!("{}\n{}\n", part_1(&races), part_2())
+    let big_race = Race {
+        time: times.join("").parse().unwrap(),
+        record: records.join("").parse().unwrap(),
+    };
+
+    format!("{}\n{}\n", part_1(&races), part_2(&big_race))
 }
 
-fn part_1(races: &[Race]) -> usize {
+fn part_1(races: &[Race]) -> u64 {
     races.iter().map(ways_to_beat).product()
 }
 
-fn part_2() -> String {
-    "part 2 unimplemented".to_string()
+fn part_2(race: &Race) -> u64 {
+    ways_to_beat(race)
 }
 
-fn ways_to_beat(race: &Race) -> usize {
-    (0..=race.time)
-        .filter(|hold_duration| hold_duration * (race.time - hold_duration) > race.record)
-        .count()
+fn ways_to_beat(race: &Race) -> u64 {
+    race.time + 1
+        - 2 * (0..=race.time)
+            .find(|hold_duration| hold_duration * (race.time - hold_duration) > race.record)
+            .unwrap()
 }
 
 struct Race {
-    time: u32,
-    record: u32,
+    time: u64,
+    record: u64,
 }
