17042fa9ff1a03f0ea8c93f567fbf42144773ae2
Šimon Brauner <simonbrauner@seznam.cz>
2024-11-30 21:50:02 +0100

    feat(13): part 2

--- src/solutions/day_13.rs
+++ src/solutions/day_13.rs
@@ -10,19 +10,27 @@ pub fn solve(input: &[String]) -> String {
         }
     }
 
-    format!("{}\n{}\n", part_1(&patterns), part_2())
+    format!("{}\n{}\n", part_1(&patterns), part_2(&patterns))
 }
 
 fn part_1(patterns: &Vec<Pattern>) -> usize {
+    summarize(patterns, 0)
+}
+
+fn part_2(patterns: &Vec<Pattern>) -> usize {
+    summarize(patterns, 1)
+}
+
+fn summarize(patterns: &Vec<Pattern>, smudge_count: usize) -> usize {
     let mut summarization = 0;
 
     for pattern in patterns {
-        if let Some(column_count) = pattern.left_column_count() {
+        if let Some(column_count) = pattern.left_column_count(smudge_count) {
             summarization += column_count;
             continue;
         }
 
-        if let Some(column_count) = pattern.up_column_count() {
+        if let Some(column_count) = pattern.up_column_count(smudge_count) {
             summarization += 100 * column_count;
             continue;
         }
@@ -31,10 +39,6 @@ fn part_1(patterns: &Vec<Pattern>) -> usize {
     }
 
     summarization
-}
-
-fn part_2() -> String {
-    "part 2 unimplemented".to_string()
 }
 
 struct Pattern {
@@ -62,20 +66,24 @@ impl Pattern {
         &self.rows[y][x]
     }
 
-    fn left_column_count(&self) -> Option<usize> {
-        'outer: for x in 1..self.x_size() {
+    fn left_column_count(&self, smudge_count: usize) -> Option<usize> {
+        for x in 1..self.x_size() {
             let mut x_up = x - 1;
             let mut x_down = x;
+            let mut mismatch_count = 0;
 
             loop {
                 for y in 0..self.y_size() {
                     if self.material_at_coords(x_up, y) != self.material_at_coords(x_down, y) {
-                        continue 'outer;
+                        mismatch_count += 1;
                     }
                 }
 
                 if x_up == 0 || x_down == self.x_size() - 1 {
-                    return Some(x);
+                    if mismatch_count == smudge_count {
+                        return Some(x);
+                    }
+                    break;
                 }
 
                 x_up -= 1;
@@ -86,20 +94,25 @@ impl Pattern {
         None
     }
 
-    fn up_column_count(&self) -> Option<usize> {
-        'outer: for y in 1..self.y_size() {
+    fn up_column_count(&self, smudge_count: usize) -> Option<usize> {
+        for y in 1..self.y_size() {
             let mut y_left = y - 1;
             let mut y_right = y;
+            let mut mismatch_count = 0;
 
             loop {
                 for x in 0..self.x_size() {
                     if self.material_at_coords(x, y_left) != self.material_at_coords(x, y_right) {
-                        continue 'outer;
+                        mismatch_count += 1;
                     }
                 }
 
                 if y_left == 0 || y_right == self.rows.len() - 1 {
-                    return Some(y);
+                    if mismatch_count == smudge_count {
+                        return Some(y);
+                    }
+
+                    break;
                 }
 
                 y_left -= 1;
