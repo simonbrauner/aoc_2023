3d04cf20e688a6c3bc1a35865db1e3ef2161bb0d
Šimon Brauner <simonbrauner@seznam.cz>
2024-12-01 10:52:24 +0100

    feat(15): part 1

--- src/solutions/day_15.rs
+++ src/solutions/day_15.rs
@@ -1,11 +1,25 @@
-pub fn solve(_input: &[String]) -> String {
-    format!("{}\n{}\n", part_1(), part_2())
+pub fn solve(input: &[String]) -> String {
+    let init_sequence: Vec<_> = input[0].split(',').collect();
+
+    format!("{}\n{}\n", part_1(&init_sequence), part_2())
 }
 
-fn part_1() -> String {
-    "part 1 unimplemented".to_string()
+fn part_1(init_sequence: &[&str]) -> usize {
+    init_sequence.iter().map(hash).sum()
 }
 
 fn part_2() -> String {
     "part 2 unimplemented".to_string()
+}
+
+fn hash(step: &&str) -> usize {
+    let mut current_value: usize = 0;
+
+    for ascii_value in step.as_bytes() {
+        current_value += *ascii_value as usize;
+        current_value *= 17;
+        current_value %= 256;
+    }
+
+    current_value
 }
