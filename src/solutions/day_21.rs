7cb2795b50cc6c308f774c7e176c9e25a73b6419
Šimon Brauner <simonbrauner@seznam.cz>
2025-06-22 13:53:31 +0000

    feat(21): part 1

--- src/solutions/day_21.rs
+++ src/solutions/day_21.rs
@@ -1,11 +1,95 @@
-pub fn solve(_input: &[String]) -> String {
-    format!("{}\n{}\n", part_1(), part_2())
+use std::collections::{HashMap, HashSet};
+
+pub fn solve(input: &[String]) -> String {
+    let mut map: HashMap<Coordinate, Tile> = HashMap::new();
+    let mut start_maybe = None;
+
+    for (y, line) in input.iter().enumerate() {
+        for (x, character) in line.chars().enumerate() {
+            let coordinate = Coordinate {
+                x: x as i32,
+                y: y as i32,
+            };
+
+            if character == 'S' {
+                start_maybe = Some(coordinate.clone());
+            }
+
+            let tile = if character == '#' {
+                Tile::Rock
+            } else {
+                Tile::Plot
+            };
+
+            map.insert(coordinate, tile);
+        }
+    }
+
+    let start = start_maybe.unwrap();
+
+    format!("{}\n{}\n", part_1(&map, &start), part_2())
 }
 
-fn part_1() -> String {
-    "part 1 unimplemented".to_string()
+fn part_1(map: &HashMap<Coordinate, Tile>, start: &Coordinate) -> usize {
+    let mut reachable = HashSet::new();
+    reachable.insert(start.clone());
+
+    for _ in 0..STEP_COUNT {
+        let mut next_reachable = HashSet::new();
+
+        for position in reachable.iter() {
+            for neighbor in position.neighbors() {
+                if let Some(tile) = map.get(&neighbor) {
+                    if *tile == Tile::Plot {
+                        next_reachable.insert(neighbor);
+                    }
+                }
+            }
+        }
+
+        reachable = next_reachable;
+    }
+
+    reachable.len()
 }
 
 fn part_2() -> String {
     "part 2 unimplemented".to_string()
 }
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct Coordinate {
+    x: i32,
+    y: i32,
+}
+
+#[derive(PartialEq)]
+enum Tile {
+    Plot,
+    Rock,
+}
+
+impl Coordinate {
+    fn neighbors(&self) -> Vec<Coordinate> {
+        vec![
+            Coordinate {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Coordinate {
+                x: self.x - 1,
+                y: self.y,
+            },
+            Coordinate {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Coordinate {
+                x: self.x,
+                y: self.y - 1,
+            },
+        ]
+    }
+}
+
+const STEP_COUNT: usize = 64;
